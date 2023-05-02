649036549a5c124da37f7d8d946ef64d6dd76e0a
Gamal <jrjb079@gmail.com>
2023-05-02 17:31:58 +0300

    test tag 1.0.0

---
+++ app/src/androidTest/java/com/example/ci_cdexample/MathHelperTest.kt
@@ -0,0 +1,25 @@
+package com.example.ci_cdexample
+
+import com.google.common.truth.Truth
+import org.junit.Test
+
+
+internal class MathHelperTest{
+
+    @Test
+    fun test_add(){
+        val result=MathHelper.add(5,6)
+        Truth.assertThat(result).isEqualTo(11)
+    }
+    @Test
+    fun test_mul(){
+        val result=MathHelper.mul(5,6)
+        Truth.assertThat(result).isEqualTo(30)
+    }
+
+    @Test
+    fun test_sub(){
+        val result=MathHelper.sub(12,6)
+        Truth.assertThat(result).isEqualTo(6)
+    }
+}
---
+++ app/src/main/java/com/example/ci_cdexample/MathHelper.kt
@@ -0,0 +1,7 @@
+package com.example.ci_cdexample
+
+object MathHelper {
+    fun add(num1: Int, num2: Int) = num1 + num2
+    fun sub(num1: Int, num2: Int) = num1 - num2
+    fun mul(num1: Int, num2: Int) = num1 * num2
+}
